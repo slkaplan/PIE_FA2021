7f1f43d00bdebd85bafdb960b0f8009a319413a9
Ian Eykamp <ian@eykamp.com>
2021-09-24 10:34:50 -0400

    Add serial communication

--- Mini_Project2/Arduino/IRscanner/IRscanner.ino
+++ Mini_Project2/Arduino/IRscanner/IRscanner.ino
@@ -2,6 +2,8 @@
 #include <RunningMedian.h>
 #include <math.h>
 
+#define DEBUG 0
+
 Servo pan_servo;
 Servo tilt_servo;
 
@@ -16,6 +18,7 @@ int IR_MIN = 20;
 int DISTANCE;
 
 #define IR_PIN A0
+#define PACKET_SIZE 3
 
 // MOVING AVERAGE
 #define WINDOW_SIZE 5
@@ -33,11 +36,13 @@ void setup() {
 
   pan_servo.attach(9);
   tilt_servo.attach(10);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // manualSet();
-  scan();
+  scanAndTransmit();
   delay(50);
 }
 
@@ -54,7 +59,7 @@ int readIR() {
 //  if (DISTANCE <= IR_MIN){
 //    DISTANCE = IR_MIN;
 //  }
-  return DISTANCE;
+  return IR_RAW;
 }
 
 int moving_avg(int data){
@@ -70,51 +75,51 @@ int moving_avg(int data){
 
 }
 
-void scan() {
-  int pan_step = 2;
-  int tilt_step = 1;
-  int n_steps = int(180 / tilt_step);
-  int tilt_sweep[n_steps];
-  int packet[3];
+// void scan() {
+//   int pan_step = 2;
+//   int tilt_step = 1;
+//   int n_steps = int(180 / tilt_step);
+//   int tilt_sweep[n_steps];
+//   int packet[3];
 
-  for (int pan_ind = 0; pan_ind < n_steps; pan_ind ++) {
-    int pan = pan_ind * tilt_step;
-    for (int tilt = 0; tilt < 180; tilt += tilt_step) {
-      packet[2] = tilt;
-      Serial.print("pan: ");
-      Serial.print(pan);
-      pan_servo.write(pan);
+//   for (int pan_ind = 0; pan_ind < n_steps; pan_ind ++) {
+//     int pan = pan_ind * tilt_step;
+//     for (int tilt = 0; tilt < 180; tilt += tilt_step) {
+//       packet[2] = tilt;
+//       // Serial.print("pan: ");
+//       // Serial.print(pan);
+//       // pan_servo.write(pan);
 
-      packet[1] = pan;
-      Serial.print(", tilt: ");
-      Serial.print(tilt);
-      tilt_servo.write(tilt);
+//       packet[1] = pan;
+//       // Serial.print(", tilt: ");
+//       // Serial.print(tilt);
+//       // tilt_servo.write(tilt);
 
-      delay(10);
+//       delay(10);
 
-      int IR = readIR();
-      packet[0] = IR;
-      Serial.print(", IR: ");
-      Serial.print(IR);
-      Serial.println("");
+//       int IR = readIR();
+//       packet[0] = IR;
+//       // Serial.print(", IR: ");
+//       // Serial.print(IR);
+//       // Serial.println("");
 
-      // tilt_sweep[pan_ind] = packet;
-    }
-    // Serial.println(sizeof(tilt_sweep));
-    // Serial.println(String(packet));
-  }
-}
+//       // tilt_sweep[pan_ind] = packet;
+//     }
+//     // Serial.println(sizeof(tilt_sweep));
+//     // Serial.println(String(packet));
+//   }
+// }
 
 void scanAndTransmit() {
   int pan_step = 2;
   int tilt_step = 1;
   int n_steps = int(180 / tilt_step);
   int tilt_sweep[n_steps];
-  int packet[3];
+  int packet[PACKET_SIZE];
 
   for (int pan_ind = 0; pan_ind < n_steps; pan_ind ++) {
     int pan = pan_ind * tilt_step;
-    send_header();
+    packetHeader();
     for (int tilt = 0; tilt < 180; tilt += tilt_step) {
       packet[1] = pan;
       packet[2] = tilt;
@@ -122,9 +127,10 @@ void scanAndTransmit() {
       tilt_servo.write(tilt);
 
       packet[0] = readIR();
-      response = sendPacket(packet);
+      bool response = sendPacket(packet);
+      digitalWrite(LED_BUILTIN, response);
     }
-    send_footer();
+    packetFooter();
   }
 }
 
@@ -186,4 +192,70 @@ bool sendPacket(int packet[PACKET_SIZE])
     }
     packetFooter();
     return checkReceived(packet);
+}
+
+
+bool checkReceived(int sentPacket[PACKET_SIZE])
+{
+    int receivedPacket[PACKET_SIZE];
+
+    while (Serial.available() <= 0)
+    {
+        if (DEBUG) {
+            Serial.print("."); // wait for a packet response
+            delay(200);
+        }
+    }
+    for (int i = 0; i < PACKET_SIZE; i++)
+    {
+        if (Serial.available() <= 0) {
+            // if the Serial buffer ends before we reach the
+            // packet size, then the handshake has failed.
+            if (DEBUG) {
+                Serial.println("Message too short");
+            }
+            return false;
+        }
+        receivedPacket[i] = Serial.read() - '0'; // the - '0' converts from an ascii character to an equivalent integer
+    }
+
+    // debugging code
+    if (DEBUG) {
+        if (!equalArrays(receivedPacket, sentPacket)) {
+            Serial.println("Message didn't match");
+        }
+        else {
+            Serial.println("Message matches!");
+        }
+
+        Serial.print("receivedPacket = ");
+        printArray(receivedPacket);
+        Serial.print(", sentPacket = ");
+        printArray(sentPacket);
+        Serial.println("");
+    }
+
+        flushBuffer();
+    return equalArrays(receivedPacket, sentPacket);
+}
+
+void printArray(int array[PACKET_SIZE]) {
+    for (int i = 0; i < PACKET_SIZE; i ++) {
+        Serial.print(array[i]);
+    }
+}
+
+bool equalArrays(int array1[PACKET_SIZE], int array2[PACKET_SIZE]) {
+    for (int i = 0; i < PACKET_SIZE; i ++) {
+        if (array1[i] != array2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void flushBuffer() {
+    while (Serial.available() > 0) {
+        Serial.read();
+    }
 }
